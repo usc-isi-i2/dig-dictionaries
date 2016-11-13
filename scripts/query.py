87660f5b5e9867dad9c7ab946c4bd9f944c2afb0
Amandeep Singh <amandeep.s.saggu@gmail.com>
2016-11-13 15:54:07 -0800

    refactory query code in a class

--- scripts/query.py
+++ scripts/query.py
@@ -1,21 +1,22 @@
 import json
-from optparse import OptionParser
 import codecs
 
 # given a query as ethinicity like Arab the function return a unnique set of all the nationality.
 # if query is in multiple categories then the result will be aggregation of all the entries.
-def getNatFomEthnicity(ethinicity, data):
-    _set = set()
-    for ele in data:
-        if ethinicity.upper() in [ e.upper() for e in ele["category"]]:
-            for nats in ele["entries"]:
-                _set.add(nats.encode("utf8"))
-    return _set
+
+class Q(object):
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def get_entries_from_category(self, ethinicity):
+        _set = set()
+        for ele in self.dictionary:
+            if ethinicity.upper() in [ e.upper() for e in ele["category"]]:
+                for nats in ele["entries"]:
+                    _set.add(nats.encode("utf8"))
+        return list(_set)
 
 if __name__ == "__main__":
-    parser = OptionParser()
-    (c_options, args) = parser.parse_args()
-    input_path = args[0]
-    data = json.load(codecs.open(input_path, 'r', 'utf-8'))
-    nationality = getNatFomEthnicity('arabs', data)
-    print nationality
+    dictionary = json.load(codecs.open('../ethnicities/ethnicity-nationality.json', 'r', 'utf-8'))
+    q = Q(dictionary)
+    print q.get_entries_from_category('arabs')
